da2622af0fafb41096c64c607e1507f212243999
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2023-05-19 19:49:40 +0600

    code updated

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,8 +1,10 @@
-import { Schema, model } from "mongoose";
-import { IUser } from "./user.interface";
+import { Model, Schema, model } from "mongoose";
+import { IUser, IUserMethods } from "./user.interface";
+
+type UserModel = Model<IUser, {}, IUserMethods>;
 
  // 2. Create a Schema corresponding to the document interface.
- const userSchema = new Schema<IUser>({
+ const userSchema = new Schema<IUser, UserModel, IUserMethods>({
 
     id: {
         type: String,
@@ -60,8 +62,11 @@ import { IUser } from "./user.interface";
     },
 
 });
+userSchema.method('fullName', function fullName() {
+    return this.name.firstName + ' ' + this.name.lastName;
+  });
 
 
 // 3. Create a Model.
-const User = model<IUser>('User', userSchema);
+const User = model<IUser,UserModel>('User', userSchema);
 export default User;
